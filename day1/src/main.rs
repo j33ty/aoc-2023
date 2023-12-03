43bddd4f58c0582101755651b59ced3b9706584d
j33ty <31261252+j33ty@users.noreply.github.com>
2023-12-03 15:17:12 +0100

    Day 1: Rust and JetBrains RustRover Part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,10 +1,22 @@
 use std::{env, fs};
+use std::collections::HashMap;
 
 // Run: cargo run -- src/input1.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
         2 => {
+            let mut digit_english = HashMap::new();
+            digit_english.insert("one", 1u32);
+            digit_english.insert("two", 2u32);
+            digit_english.insert("three", 3u32);
+            digit_english.insert("four", 4u32);
+            digit_english.insert("five", 5u32);
+            digit_english.insert("six", 6u32);
+            digit_english.insert("seven", 7u32);
+            digit_english.insert("eight", 8u32);
+            digit_english.insert("nine", 9u32);
+
             let file_path = &args[1];
             let file = fs::read_to_string(file_path).
                 expect("failed to read file");
@@ -12,20 +24,32 @@ fn main() {
 
             let mut sum: u32 = 0;
             lines.for_each(|line| {
-                let (mut first_digit, mut last_digit): (Option<u32>, Option<u32>) = (None, None);
-                for c in line.chars() {
+                let (mut first_digit, mut last_digit): (Option<(u32, u32)>, Option<(u32, u32)>) = (None, None);
+                for (i, c) in line.chars().enumerate() {
                     if c.is_digit(10) {
                         if first_digit.is_none() {
-                            first_digit = Some(c.to_digit(10).unwrap());
+                            first_digit = Some((c.to_digit(10).unwrap(), i as u32));
                         }
-                        last_digit = Some(c.to_digit(10).unwrap());
+                        last_digit = Some((c.to_digit(10).unwrap(), i as u32));
+                    }
+                }
+
+                for (digit_name, digit_val) in &digit_english {
+                    let first_instance = line.find(digit_name);
+                    if first_instance.is_some() && (first_digit.is_none() || first_digit.unzip().1.unwrap() > first_instance.unwrap() as u32) {
+                        first_digit = Option::from((digit_val.clone(), first_instance.unwrap() as u32))
+                    }
+
+                    let last_instance = line.rfind(digit_name);
+                    if last_instance.is_some() && (last_digit.is_none() || last_digit.unzip().1.unwrap() < last_instance.unwrap() as u32) {
+                        last_digit = Option::from((digit_val.clone(), last_instance.unwrap() as u32))
                     }
                 }
 
                 if first_digit.is_some() && last_digit.is_some() {
-                    sum += 10 * first_digit.unwrap() + last_digit.unwrap()
+                    sum += 10 * first_digit.unwrap().0 + last_digit.unwrap().0
                 } else if first_digit.is_some() && last_digit.is_none() {
-                    sum += first_digit.unwrap()
+                    sum += first_digit.unwrap().0
                 }
             });
             println!("{sum}");
